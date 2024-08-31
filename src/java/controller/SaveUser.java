bf62c4961ec29182f59f897bcaea71e20651cd29
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-08-31 12:37:20 +0530

    Fully working with DBMS
    📌User.java 👇
    
    package model;
    
    import java.io.Serializable;
    import javax.persistence.Column;
    import javax.persistence.Entity;
    import javax.persistence.Id;
    import javax.persistence.Table;
    
    @Entity
    @Table(name = "user")
    public class User implements Serializable {
    
        @Id
        @Column(name = "id")
        @GeneratedValue(strategy = GenerationType.IDENTITY) //optional
        private int id;
    
        @Column(name = "name",length = 50,nullable = false)
        private String name;
    
        @Column(name = "mobile",length = 10,nullable = false)
        private String mobile;
    
        public User() {
        }
    
        ......getters and setters......
    
    -------------------------------------------------------------
    
    📌SaveUser.java Servlet 👇
    
    package controller;
    
    import java.io.IOException;
    import javax.servlet.ServletException;
    import javax.servlet.annotation.WebServlet;
    import javax.servlet.http.HttpServlet;
    import javax.servlet.http.HttpServletRequest;
    import javax.servlet.http.HttpServletResponse;
    import model.HibernateUtil;
    import model.User;
    import org.hibernate.Session;
    import org.hibernate.SessionFactory;
    
    @WebServlet(name = "SaveUser", urlPatterns = {"/SaveUser"})
    public class SaveUser extends HttpServlet {
    
        @Override
        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
    
            SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
            Session session = sessionFactory.openSession();
    
            User user = new User();
            user.setName("Tharaka");
            user.setMobile("0710000124");
    
            session.save(user);
            session.beginTransaction().commit();
    
        }
    
    }
    
    ----------------------------------------------------------------
    
    📌hibernate.cfg.xml 👇
    
    add below line :-
    
    <mapping class="model.User"/>
    
    ------------------------------------------------------

---
+++ src/java/controller/SaveUser.java
@@ -0,0 +1,32 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.HibernateUtil;
+import model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+@WebServlet(name = "SaveUser", urlPatterns = {"/SaveUser"})
+public class SaveUser extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        
+        User user = new User();
+        user.setName("Muriya");
+        user.setMobile("0710000125");
+        
+        session.save(user);
+        session.beginTransaction().commit();
+        
+    }
+
+}

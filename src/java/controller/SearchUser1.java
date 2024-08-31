c68aa1372cb1901b42df6bf43fabf5ff2ecaa210
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-08-31 15:13:24 +0530

    Criteria Search
    public class SearchUser1 extends HttpServlet {
    
        @Override
        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
    
            SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
            Session session = sessionFactory.openSession();
    
            //SELECT * FROM `user`
            Criteria criteria = session.createCriteria(User.class);
            ArrayList<User> userList = (ArrayList<User>) criteria.list();
    
            for (User user : userList) {
                System.out.println(user.getName());
            }
            session.close();
        }
    
    }

--- src/java/controller/SearchUser1.java
+++ src/java/controller/SearchUser1.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.HibernateUtil;
 import model.User;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -20,12 +22,13 @@ public class SearchUser1 extends HttpServlet {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         Session session = sessionFactory.openSession();
 
-        User user =(User) session.load(User.class,5);
-        
-        
-        session.delete(user);
-        
-        session.beginTransaction().commit();
+        //SELECT * FROM user'
+        Criteria criteria = session.createCriteria(User.class);
+        ArrayList<User> userList = (ArrayList<User>) criteria.list();
+
+        for (User user : userList) {
+            System.out.println(user.getName());
+        }
         session.close();
     }
 
